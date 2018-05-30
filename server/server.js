5a8a46b90ddd281e6950febabcc2f0ffb080be95
testboy <alcochtivo@gmail.com>
2018-05-30 15:52:41 +0500

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb')
 let {mongoose} = require('./db/mongoose')
 let {Todo} = require('./models/todo')
 let {User} = require('./models/user')
+let {authenticate} = require('./middleware/authenticate')
 
 let app = express()
 
@@ -21,19 +22,6 @@ app.post('/todos', (req, res) => {
     res.send(doc)
   }, (e) => {
     res.status(400).send()
-  })
-})
-
-app.post('/users', (req, res) => {
-  let body = _.pick(req.body, ['email', 'password'])
-  let user = new User(body)
-
-  user.save().then(() => {
-    return user.generateAuthToken()
-  }).then((token) => {
-    res.header('x-auth', token).send(user)
-  }).catch((e) => {
-    res.status(400).send(e.message)
   })
 })
 
@@ -94,6 +82,23 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send()
   })
+})
+
+app.post('/users', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password'])
+  let user = new User(body)
+
+  user.save().then(() => {
+    return user.generateAuthToken()
+  }).then((token) => {
+    res.header('x-auth', token).send(user)
+  }).catch((e) => {
+    res.status(400).send(e.message)
+  })
+})
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user)
 })
 
 app.listen(process.env.PORT, () => {
